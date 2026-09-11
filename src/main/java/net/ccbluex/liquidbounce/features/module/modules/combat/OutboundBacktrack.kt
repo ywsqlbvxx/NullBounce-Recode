2a67f456a6f63998032a9fdfab54926d161700f8
Gi9292 <bejaranovieragustavo@gmail.com>
2026-09-11 15:04:40 -0400

    Update OutboundBacktrack.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/OutboundBacktrack.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/OutboundBacktrack.kt
@@ -1,4 +1,5 @@
 /*
+/*
  * LiquidBounce Hacked Client
  * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
  * https://github.com/CCBlueX/LiquidBounce/
@@ -201,3 +202,4 @@ object OutboundBacktrack : Module("OutboundBacktrack", Category.COMBAT, gameDete
         }
     }
 }
+/*
